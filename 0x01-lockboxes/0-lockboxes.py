60e22bb102148b4b47babcc9b6fa7f782919cba4
soloking44 <solop8483@gmail.com>
2024-07-02 00:58:23 -0700

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Creates a function to check if a box with nested lists
+   can be unlocked using keys found within those lists.
+"""
+
+
+def canUnlockAll(boxes):
+    """This checks if boxes can be opened"""
+    position = 0
+    unlocked = {}
+
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False
