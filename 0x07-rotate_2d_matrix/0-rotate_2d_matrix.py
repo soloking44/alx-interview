3ca16539109773b5d71526676e108d1cabc152be
soloking44 <solop8483@gmail.com>
2024-08-13 02:05:28 -0700

    0-rotate_2d_matrix.py

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A process of rotation method.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotates the 2D matrix into place.
+    """
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    c, r = 0, rows - 1
+    for p in range(cols * rows):
+        if p % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
